83cb36b34e9aa7bbd4dc7b42ad46ca22152813bb
Merouane Bellaha <m.bellaha@gmail.com>
2020-11-08 21:23:12 +0100

    Initial Commit

---
+++ OnboardingDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardingDemo
+//
+//  Created by Merouane Bellaha on 08/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnboardingDemo/OnboardingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingDemoApp.swift
+//  OnboardingDemo
+//
+//  Created by Merouane Bellaha on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
